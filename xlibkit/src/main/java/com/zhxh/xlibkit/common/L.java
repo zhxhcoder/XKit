8b40742b31c118bf2fe9345b7a42cc3e1a4ed1b8
zhxh <zhxhcoder@gmail.com>
2018-09-11 16:01:04 +0800

    日志输出

---
+++ xlibkit/src/main/java/com/zhxh/xlibkit/common/L.java
@@ -0,0 +1,176 @@
+package com.zhxh.xlibkit.common;
+
+import android.util.Log;
+
+import com.zhxh.xlibkit.BuildConfig;
+
+/**
+ * Created by zhxh on 2018/9/11
+ */
+public class L {
+    public final static boolean isDebugDevice = true;
+    public final static String defaultTag = "defaultTag";
+    static String TAG;
+    static String fileName;
+    static String className;
+    static String methodName;
+    static int lineNumber;
+
+    private L() {
+        /* Protect from instantiations */
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    public static boolean isDebuggable() {
+        return BuildConfig.DEBUG;
+    }
+
+    private static String createLog(String log) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        stringBuilder.append(fileName);
+        stringBuilder.append(" ");
+        //stringBuilder.append(className);
+        //stringBuilder.append(" ");
+        stringBuilder.append(methodName);
+        stringBuilder.append(":");
+        stringBuilder.append(lineNumber);
+        stringBuilder.append("]");
+        stringBuilder.append("->");
+        stringBuilder.append(log);
+        return stringBuilder.toString();
+    }
+
+    /**
+     * debug info
+     *
+     * @param sElements
+     */
+    private static void getMethodNames(StackTraceElement[] sElements) {
+        /**
+         * 每输出一个日志前都要先别 TAG还原
+         */
+        TAG = defaultTag;
+        fileName = sElements[1].getFileName();
+        className = sElements[1].getClassName();
+        methodName = sElements[1].getMethodName();
+        lineNumber = sElements[1].getLineNumber();
+    }
+
+    public static void e(String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        Log.e(TAG, createLog(message));
+    }
+
+    public static void i(String message) {
+        if (!isDebuggable())
+            return;
+        getMethodNames(new Throwable().getStackTrace());
+        Log.i(TAG, createLog(message));
+    }
+
+    public static void d(String message) {
+        if (!isDebuggable())
+            return;
+        getMethodNames(new Throwable().getStackTrace());
+        Log.d(TAG, createLog(message));
+    }
+
+    public static void v(String message) {
+        if (!isDebuggable())
+            return;
+        getMethodNames(new Throwable().getStackTrace());
+        Log.v(TAG, createLog(message));
+    }
+
+    public static void w(String message) {
+        if (!isDebuggable())
+            return;
+        getMethodNames(new Throwable().getStackTrace());
+        Log.w(TAG, createLog(message));
+    }
+
+    public static void wtf(String message) {
+        if (!isDebuggable())
+            return;
+        getMethodNames(new Throwable().getStackTrace());
+        Log.wtf(TAG, createLog(message));
+    }
+
+    /********************************************************/
+    public static void e(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.e(TAG, createLog(message));
+    }
+
+    public static void i(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.i(TAG, createLog(message));
+    }
+
+    public static void d(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.d(TAG, createLog(message));
+    }
+
+    public static void v(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.v(TAG, createLog(message));
+    }
+
+    public static void w(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.w(TAG, createLog(message));
+    }
+
+    public static void wtf(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.wtf(TAG, createLog(message));
+    }
+}
