4be50b54d88765a03e7244e700cddb7541d25e02
zhxh <zhxhcoder@gmail.com>
2018-09-11 16:07:39 +0800

    日志输出优化

--- xlibkit/src/main/java/com/zhxh/xlibkit/common/LogUtil.java
+++ xlibkit/src/main/java/com/zhxh/xlibkit/common/LogUtil.java
@@ -8,74 +8,169 @@ import com.zhxh.xlibkit.BuildConfig;
  * Created by zhxh on 2018/8/29
  */
 public final class LogUtil {
-    private static String TAG = "debugLog";
+    private final static String defaultTag = "defaultLog";
+    static String TAG;
+    static String fileName;
+    static String className;
+    static String methodName;
+    static int lineNumber;
+
+    private LogUtil() {
+        /* Protect from instantiations */
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    private static boolean isDebuggable() {
+        return BuildConfig.DEBUG;
+    }
+
+    private static String createLog(String log) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        stringBuilder.append(fileName);
+        stringBuilder.append(" ");
+        //stringBuilder.append(className);
+        //stringBuilder.append(" ");
+        stringBuilder.append(methodName);
+        stringBuilder.append(":");
+        stringBuilder.append(lineNumber);
+        stringBuilder.append("]");
+        stringBuilder.append("->");
+        stringBuilder.append(log);
+        return stringBuilder.toString();
+    }
+
     /**
-     * 日志输出级别 0为不输出
+     * debug info
+     *
+     * @param sElements
      */
-    private static int logMode = BuildConfig.DEBUG ? 4 : 4;
-
-    public static void e(String tag, String msg) {
-        if (tag == null || msg == null) {
-            return;
-        }
-        if (Log.ERROR <= logMode)
-            Log.e(tag, msg);
+    private static void getMethodNames(StackTraceElement[] sElements) {
+        /**
+         * 每输出一个日志前都要先别 TAG还原
+         */
+        TAG = defaultTag;
+        fileName = sElements[1].getFileName();
+        className = sElements[1].getClassName();
+        methodName = sElements[1].getMethodName();
+        lineNumber = sElements[1].getLineNumber();
     }
 
-    public static void w(String tag, String msg) {
-        if (tag == null || msg == null) {
+    public static void e(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.WARN <= logMode)
-            Log.w(tag, msg);
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        Log.e(TAG, createLog(message));
     }
 
-    public static void i(String tag, String msg) {
-        if (tag == null || msg == null) {
+    public static void i(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.INFO <= logMode)
-            Log.i(tag, msg);
+        getMethodNames(new Throwable().getStackTrace());
+        Log.i(TAG, createLog(message));
     }
 
-    public static void i(String msg) {
-        if (msg == null) {
+    public static void d(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.INFO <= logMode)
-            Log.i(TAG, msg);
+        getMethodNames(new Throwable().getStackTrace());
+        Log.d(TAG, createLog(message));
     }
 
-    public static void d(String tag, String msg) {
-        if (tag == null || msg == null) {
+    public static void v(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.DEBUG <= logMode)
-            Log.d(tag, msg);
+        getMethodNames(new Throwable().getStackTrace());
+        Log.v(TAG, createLog(message));
     }
 
-    public static void d(String msg) {
-        if (msg == null) {
+    public static void w(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.DEBUG <= logMode)
-            Log.d(TAG, msg);
+        getMethodNames(new Throwable().getStackTrace());
+        Log.w(TAG, createLog(message));
     }
 
-    public static void e(Object object, String msg) {
-        if (msg == null) {
+    public static void wtf(String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.DEBUG <= logMode)
-            Log.e(object.getClass().getSimpleName(), msg);
+        getMethodNames(new Throwable().getStackTrace());
+        Log.wtf(TAG, createLog(message));
     }
 
-    public static void e(String msg) {
-        if (msg == null) {
+    /********************************************************/
+    public static void e(String tag, String message) {
+        if (!isDebuggable())
             return;
-        }
-        if (Log.DEBUG <= logMode)
-            Log.e(TAG, msg);
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.e(TAG, createLog(message));
+    }
+
+    public static void i(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.i(TAG, createLog(message));
+    }
+
+    public static void d(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.d(TAG, createLog(message));
+    }
+
+    public static void v(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.v(TAG, createLog(message));
+    }
+
+    public static void w(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.w(TAG, createLog(message));
+    }
+
+    public static void wtf(String tag, String message) {
+        if (!isDebuggable())
+            return;
+        // Throwable instance must be created before any methods
+        getMethodNames(new Throwable().getStackTrace());
+        /**
+         * 默认从 getMethodNames里得到Tag
+         */
+        TAG = tag;
+        Log.wtf(TAG, createLog(message));
     }
 }
 
